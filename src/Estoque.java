70fb09638126d4a4ecaa7429381c58128d4357e9
Fernanda Souza Oliveira <fernanda.oliveira@dbserver.com.br>
2026-01-05 20:18:27 -0300

    Funcão para inicializar Estoque

--- src/Estoque.java
+++ src/Estoque.java
@@ -5,7 +5,22 @@ public class Estoque {
 
 
     public Estoque(){
+
         this.listaDeProdutos = new ArrayList<>();
+        inicializaEstoque();
+    }
+
+    public void inicializaEstoque(){
+        listaDeProdutos.add(new Produto(1, "Arroz", 4.80, 10));
+        listaDeProdutos.add(new Produto(2, "Feijão", 6.50, 10));
+        listaDeProdutos.add(new Produto(3, "Café Melita", 22.30, 5));
+        listaDeProdutos.add(new Produto(4, "Café Premiun", 35.00, 2));
+        listaDeProdutos.add(new Produto(5, "Coca Cola", 10.99, 20));
+        listaDeProdutos.add(new Produto(6, "Chocolate Preto", 6.00, 13));
+        listaDeProdutos.add(new Produto(7, "Chocolate Branco", 4.99, 17));
+        listaDeProdutos.add(new Produto(8, "Presunto", 3.30, 10));
+        listaDeProdutos.add(new Produto(9, "Queijo", 7.00, 10));
+        listaDeProdutos.add(new Produto(10, "Pão de Sanduiche", 9.00, 5));
     }
 
 
