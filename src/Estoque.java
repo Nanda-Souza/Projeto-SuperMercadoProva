77cd2462e0892a2af96598aa93dd33abe3832233
Fernanda Souza Oliveira <fernanda.oliveira@dbserver.com.br>
2026-01-05 19:21:02 -0300

    Classe Estoque

--- src/Estoque.java
+++ src/Estoque.java
@@ -1,2 +1,14 @@
+import java.util.ArrayList;
+
 public class Estoque {
+    private ArrayList<Produto> listaDeProdutos;
+
+
+    public Estoque(){
+        this.listaDeProdutos = new ArrayList<>();
+    }
+
+
+
+
 }
