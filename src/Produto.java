3a6a714b4b51172dd6429b8077fc04901e9f78ca
Fernanda Souza Oliveira <fernanda.oliveira@dbserver.com.br>
2026-01-05 18:43:56 -0300

    Metodos e contrutor da classe Produto

--- src/Produto.java
+++ src/Produto.java
@@ -1,2 +1,46 @@
 public class Produto {
+    private Integer id;
+    private String nome;
+    private Double preco;
+    private Integer quantidadeEmEstoque;
+
+    public Produto(int id, String nome, double preco, int quantidadeEmEstoque){
+        this.id = id;
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidadeEmEstoque = quantidadeEmEstoque;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    public double getPreco(){
+        return preco;
+    }
+
+    public int getQuantidadeEmEstoque(){
+        return quantidadeEmEstoque;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    public void setQuantidadeEmEstoque(Integer quantidadeEmEstoque) {
+        this.quantidadeEmEstoque = quantidadeEmEstoque;
+    }
+
 }
