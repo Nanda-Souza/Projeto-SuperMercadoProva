0b88f776db495988479c5c1cb2878641cb7f6108
Fernanda Souza Oliveira <fernanda.oliveira@dbserver.com.br>
2026-01-06 18:00:18 -0300

    Fluxo para produto

--- src/SuperMercado.java
+++ src/SuperMercado.java
@@ -1,10 +1,28 @@
 import java.util.Scanner;
 
+
 public class SuperMercado {
     public static boolean inputValido(String input) {
         return input != null &&
                 input.matches("^[12]$");
     }
+
+    public static boolean nomeDeProdutoValido(String nomeProduto) {
+        return nomeProduto != null &&
+                nomeProduto.matches("^(?=.*[A-Za-zÀ-ÿ])[A-Za-zÀ-ÿ0-9 .,\\\\-]+$") &&
+                nomeProduto.length() > 1;
+    }
+
+    public static boolean precoValido(String preco){
+        return preco != null &&
+                preco.matches("^\\d+,\\d{2}$");
+    }
+
+    public static boolean quantidadeValida(String quantidade) {
+        return quantidade != null &&
+                quantidade.matches("\\d+");
+    }
+
     public static void main(String[] args) {
         //Variaveis de Controle
         Scanner scanner = new Scanner(System.in);
@@ -51,22 +69,67 @@ public class SuperMercado {
                     break;
 
                 case "2":
-                    System.out.println("\nInsira o nome to produto: ");
-                    nomeProduto = scanner.nextLine().trim();
 
-                    System.out.println("\nInsira o preço do produto: ");
-                    precoProduto = scanner.nextDouble();
+                    do {
+                        System.out.println("\nInsira o nome to produto: ");
+                        nomeProduto = scanner.nextLine().trim();
 
-                    System.out.println("\nInsira a quantidade em estoque: ");
-                    quantidadeProduto = scanner.nextInt();
+                        while(!nomeDeProdutoValido(nomeProduto)){
+                            System.out.println("Nome do produto inválido, digite um nome de produto com no mínimo 2 caracteres e ao menos uma letra:");
+                            nomeProduto = scanner.nextLine().trim();
+                        }
 
-                    Produto p = new Produto(idProduto++, nomeProduto, precoProduto, quantidadeProduto);
+                        System.out.println("\nInsira o preço do produto: ");
+                        inputUsuario = scanner.nextLine().trim();
 
-                    if (estoque.cadastrarProduto(p)){
-                        System.out.println("Produto cadastrado com sucesso!");
-                    } else {
-                        System.out.println("Cadastro não realizado pois o produto já existe!");
-                    }
+                        while(!precoValido(inputUsuario)){
+                            System.out.println("Preço inválido. Digite apenas números, usando vírgula para separar os centavos (ex: 100,20):");
+                            inputUsuario = scanner.nextLine().trim();
+                        }
+
+                        precoProduto = Double.parseDouble(inputUsuario.replace(",", "."));
+
+                        System.out.println("\nInsira a quantidade em estoque: ");
+                        inputUsuario = scanner.nextLine().trim();
+
+                        while(!quantidadeValida(inputUsuario)){
+                            System.out.println("Quantidade inválida. Digite apenas um numero inteiro:");
+                            inputUsuario = scanner.nextLine().trim();
+
+                        }
+
+                        quantidadeProduto = Integer.parseInt(inputUsuario);
+
+                        Produto p = new Produto(idProduto++, nomeProduto, precoProduto, quantidadeProduto);
+
+                        if (estoque.cadastrarProduto(p)){
+                            System.out.println("Produto cadastrado com sucesso. Deseja cadastrar um novo produto?");
+                            System.out.println("1 - Sim");
+                            System.out.println("2 - Não");
+
+                            inputUsuario = scanner.nextLine().trim();
+
+                            while (!inputValido(inputUsuario)){
+                                System.out.println("\nOpção inválida, digite 1 para sim e 2 para não:");
+                                inputUsuario = scanner.nextLine().trim();
+                            }
+
+                        } else {
+                            System.out.println("Produto já cadastrado! Deseja cadastrar um novo produto?");
+                            System.out.println("1 - Sim");
+                            System.out.println("2 - Não");
+
+                            inputUsuario = scanner.nextLine().trim();
+
+                            while (!inputValido(inputUsuario)){
+                                System.out.println("\nOpção inválida, digite 1 para sim e 2 para não:");
+                                inputUsuario = scanner.nextLine().trim();
+                            }
+
+                        }
+
+                    } while (inputUsuario.equals("1"));
+
                     break;
 
                 case "3":
